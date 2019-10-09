ff196c75e87c8abdb467a14f590217b46c5b7400
guilherme_j <guilherme_j@grupocriar>
2019-10-09 18:02:13 -0300

    erros

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './Global.css';
 
 import Header from './components/header/header';
+import PostList from './components/posts/postList';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <PostList />
+    </>
   );
 }
 
